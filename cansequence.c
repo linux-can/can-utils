b3b501d92222ee91a37de742641a2f3e903169fa
Marc Kleine-Budde <mkl@pengutronix.de>
2008-08-26 15:45:01 +0000

    git-svn-id: https://iocaste.extern.pengutronix.de/svn/canutils/trunks/canutils-3.0-trunk@88 5fd5a299-6ef2-0310-aa18-8b01d7c39d8c

--- cansequence.c
+++ cansequence.c
@@ -1,22 +1,22 @@
 #include <can_config.h>
 
+#include <getopt.h>
+#include <libgen.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <signal.h>
-#include <libgen.h>
-#include <getopt.h>
+#include <unistd.h>
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/uio.h>
-#include <sys/ioctl.h>
 #include <net/if.h>
+
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/uio.h>
 
 #include <linux/can.h>
 #include <linux/can/raw.h>
-#include <linux/can/ioctl.h>
 
 extern int optind, opterr, optopt;
 
