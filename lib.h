8db365d2852e9a0ddc4f6a1a759b0bfa12b42dab
Oliver Hartkopp <socketcan@hartkopp.net>
2013-02-15 23:39:32 +0100

    lib: fix typo: exended -> extended
    Signed-off-by: Oliver Hartkopp <socketcan@hartkopp.net>

--- lib.h
+++ lib.h
@@ -152,9 +152,9 @@ void sprint_canframe(char *buf , struct canfd_frame *cf, int sep, int maxdlen);
  * maxdlen = 8 -> CAN2.0 frame
  * maxdlen = 64 -> CAN FD frame
  *
- * 12345678#112233 -> exended CAN-Id = 0x12345678, len = 3, data, sep = 0
- * 12345678#R -> exended CAN-Id = 0x12345678, RTR, len = 0
- * 12345678#R5 -> exended CAN-Id = 0x12345678, RTR, len = 5
+ * 12345678#112233 -> extended CAN-Id = 0x12345678, len = 3, data, sep = 0
+ * 12345678#R -> extended CAN-Id = 0x12345678, RTR, len = 0
+ * 12345678#R5 -> extended CAN-Id = 0x12345678, RTR, len = 5
  * 123#11.22.33.44.55.66.77.88 -> standard CAN-Id = 0x123, dlc = 8, sep = 1
  * 32345678#112233 -> error frame with CAN_ERR_FLAG (0x2000000) set
  * 123##0112233 -> CAN FD frame standard CAN-Id = 0x123, flags = 0, len = 3
@@ -184,11 +184,11 @@ void sprint_long_canframe(char *buf , struct canfd_frame *cf, int view, int maxd
  * maxdlen = 8 -> CAN2.0 frame
  * maxdlen = 64 -> CAN FD frame
  *
- * 12345678   [3]  11 22 33 -> exended CAN-Id = 0x12345678, dlc = 3, data
- * 12345678   [0]  remote request -> exended CAN-Id = 0x12345678, RTR
+ * 12345678   [3]  11 22 33 -> extended CAN-Id = 0x12345678, dlc = 3, data
+ * 12345678   [0]  remote request -> extended CAN-Id = 0x12345678, RTR
  * 14B0DC51   [8]  4A 94 E8 2A EC 58 55 62   'J..*.XUb' -> (with ASCII output)
  * 20001111   [7]  C6 23 7B 32 69 98 3C      ERRORFRAME -> (CAN_ERR_FLAG set)
- * 12345678  [03]  11 22 33 -> CAN FD with exended CAN-Id = 0x12345678, dlc = 3
+ * 12345678  [03]  11 22 33 -> CAN FD with extended CAN-Id = 0x12345678, dlc = 3
  *
  * 123   [3]  11 22 33         -> CANLIB_VIEW_INDENT_SFF == 0
  *      123   [3]  11 22 33    -> CANLIB_VIEW_INDENT_SFF == set
