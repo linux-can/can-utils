e907f3c78a6e89e996442c4b26173c03312baaf8
Toon Stegen <toon.stegen@altran.com>
2016-06-28 16:59:49 +0200

    Change license of can-calc-bittiming
    All copyright holders agreed to change the license of this file to
    LGPLv2 so this bittiming calculation function can be used other LGPL
    licensed open source projects.

--- can-calc-bit-timing.c
+++ can-calc-bit-timing.c
@@ -10,10 +10,10 @@
  *   Copyright 2005      Stanislav Marek
  *   email:pisa@cmp.felk.cvut.cz
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  */
 
 #include <errno.h>
