4664e69ca11f4098bbba3d04c8ed72e121264040
Christopher McClellan <cmcclellan@pillartechnology.com>
2018-01-16 09:59:07 -0500

    Android.mk: Allow building in AOSP source tree
    AOSP is treating all warnings as errors. Allow these specific warnings through.

--- Android.mk
+++ Android.mk
@@ -3,7 +3,11 @@ LOCAL_PATH := $(call my-dir)
 PRIVATE_LOCAL_CFLAGS := -O2 -g -W -Wall		\
 			-DSO_RXQ_OVFL=40	\
 			-DPF_CAN=29		\
-			-DAF_CAN=PF_CAN
+			-DAF_CAN=PF_CAN \
+			-Wno-error=unused-parameter \
+			-Wno-error=sign-compare \
+			-Wno-error=pointer-arith \
+			-Wno-error=address-of-packed-member
 
 #
 # canlib
