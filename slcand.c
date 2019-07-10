59f92965b90efc00b0cb63e3d4edfefd9b24326f
Yegor Yefremov <yegorslists@googlemail.com>
2019-07-10 13:18:10 +0200

    Relicense GPL-2.0-or-later files to GPL-2.0-only
    This way all licenses in the project will be homogenised.
    
    Acked-by: Marc Kleine-Budde <mkl@pengutronix.de>
    Acked-by: Oliver Hartkopp <socketcan@hartkopp.net>
    Acked-by: Sven Schmitt <sven.schmitt@gmx.net>
    Acked-by: Oleksij Rempel <ore@pengutronix.de>
    Acked-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Acked-by: Brennan Ashton <bashton@brennanashton.com>
    Acked-By: IOhannes m zmölnig <zmoelnig@iem.at>
    Acked-by: Frank Theile <ftheile@grundfos.com>
    Acked-by: Wolfgang Grandegger <wg@grandegger.com>
    Acked-by: Jeremiah Mahler <jmmahler@gmail.com>
    Acked-by: Jonathan Challinger <mr.challinger@gmail.com>
    Acked-by: Cheng-Lung Lee <chenglung@gmail.com>
    Acked-by: Ramesh Shanmugasundaram <rashanmu@gmail.com>
    Acked-by: Mans Rullgard <mans@mansr.com>
    Acked-by: Søren Holm <sgh@sgh.dk>
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>

--- slcand.c
+++ slcand.c
@@ -1,4 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+/* SPDX-License-Identifier: GPL-2.0-only */
 /*
  * slcand.c - userspace daemon for serial line CAN interface driver SLCAN
  *
@@ -6,9 +6,8 @@
  * Copyright (c) 2009 Verari Systems Inc.
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * it under the terms of the version 2 of the GNU General Public License
+ * as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
