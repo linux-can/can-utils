73e421557faa40161700da5e8a7378f840889c35
Marc Kleine-Budde <mkl@pengutronix.de>
2020-09-17 12:46:32 +0200

    canfdtest: remove trailing whitespace
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- canfdtest.c
+++ canfdtest.c
@@ -203,7 +203,7 @@ static int check_frame(const struct can_frame *frame)
 {
 	int err = 0;
 	int i;
-	
+
 	if (frame->can_id != CAN_MSG_ID) {
 		printf("unexpected Message ID 0x%04x!\n", frame->can_id);
 		err = -1;
@@ -213,7 +213,7 @@ static int check_frame(const struct can_frame *frame)
 		printf("unexpected Message length %d!\n", frame->can_dlc);
 		err = -1;
 	}
-	
+
 	for (i = 1; i < frame->can_dlc; i++) {
 		if (frame->data[i] != (uint8_t)(frame->data[i-1] + 1)) {
 			printf("Frame inconsistent!\n");
