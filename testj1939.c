204235c9ceaa8fdd598e6306fb54311f2a0483ee
Rosen Penev <rosenp@gmail.com>
2020-10-13 15:21:28 -0700

    make struct initialization C99 compatible again
    Mainly change {} to { 0 } to remove a pedantic warning.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- testj1939.c
+++ testj1939.c
@@ -61,7 +61,7 @@ static void onsigalrm(int sig)
 
 static void schedule_oneshot_itimer(double delay)
 {
-	struct itimerval it = {};
+	struct itimerval it = { 0 };
 
 	it.it_value.tv_sec = delay;
 	it.it_value.tv_usec = (long)(delay * 1e6) % 1000000;
