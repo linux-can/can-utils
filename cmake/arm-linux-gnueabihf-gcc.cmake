4597b054e2be71ca3a677037febac5ddf1ed4c01
Marc Kleine-Budde <mkl@pengutronix.de>
2024-05-29 11:26:28 +0200

    github-actions: add cross-compiling

---
+++ cmake/arm-linux-gnueabihf-gcc.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_C_COMPILER_TARGET arm-linux-gnueabihf)
---
+++ cmake/mips-linux-gnu-gcc.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR mips)
+
+set(CMAKE_C_COMPILER mips-linux-gnu-gcc)
+set(CMAKE_C_COMPILER_TARGET mips-linux-gnu)
