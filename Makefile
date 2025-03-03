linux-can/can-utils

commit 71b2aec834e74d57c643e7a6776f21a1448ef17a

j1939-vehicle-position-srv: Introduce J1939 and NMEA 2000 Vehicle Position Server
This patch adds `j1939-vehicle-position-srv`, a server for sending
vehicle position data over CAN using J1939 or NMEA 2000 protocols. It
retrieves GPS data from gpsd or simulates data if gpsd is unavailable.
By default, it operates in J1939 profile but can switch to NMEA 2000
with the `-p nmea2000` option.

Usage Examples:
1. With gpsd:
   j1939acd -r 64-95 -c /tmp/1122334455667789.jacd 1122334455667789 vcan0 &
   j1939-vehicle-position-srv -i vcan0 -n 0x1122334455667789

2. In simulation mode without gpsd:
   j1939-vehicle-position-srv -i vcan0 -s -p nmea2000

Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>
[Yegor: add CMakeLists.txt integration]
Co-developed-by: Yegor Yefremov <yegorslists@googlemail.com>

Makefile
@@ -67,6 +67,9 @@ PROGRAMS_J1939_TIMEDATE := \
 	j1939-timedate-srv \
 	j1939-timedate-cli
 
+PROGRAMS_J1939_VEHICLE_POSITION := \
+	j1939-vehicle-position-srv
+
 PROGRAMS_ISOBUSFS := \
 	isobusfs-srv \
 	isobusfs-cli
@@ -98,6 +101,7 @@ PROGRAMS_SLCAN := \
 PROGRAMS := \
 	$(PROGRAMS_CANGW) \
 	$(PROGRAMS_J1939_TIMEDATE) \
+	$(PROGRAMS_J1939_VEHICLE_POSITION) \
 	$(PROGRAMS_ISOBUSFS) \
 	$(PROGRAMS_ISOTP) \
 	$(PROGRAMS_J1939) \
@@ -126,7 +130,8 @@ endif
 all: $(PROGRAMS)
 
 clean:
-	rm -f $(PROGRAMS) *.o mcp251xfd/*.o isobusfs/*.o j1939_timedate/*.o
+	rm -f $(PROGRAMS) *.o mcp251xfd/*.o isobusfs/*.o j1939_timedate/*.o \
+		j1939_vehicle_position/*.o
 
 install:
 	mkdir -p $(DESTDIR)$(PREFIX)/bin
@@ -153,6 +158,8 @@ isobusfs_srv.o:	lib.h libj1939.h
 isobusfs_c.o:	lib.h libj1939.h
 j1939_timedate_srv.o: lib.h libj1939.h
 j1939_timedate_cli.o: lib.h libj1939.h
+j1939_vehicle_position_srv.o: lib.h libj1939.h
+
 canframelen.o:  canframelen.h
 
 asc2log:	asc2log.o	lib.o
@@ -181,6 +188,12 @@ j1939-timedate-cli:	lib.o \
 			libj1939.o \
 			j1939_timedate/j1939_timedate_cli.o
 		$(CC) $(LDFLAGS) $^ $(LDLIBS) -o $@
+
+j1939-vehicle-position-srv: \
+			lib.o \
+			libj1939.o \
+			j1939_vehicle_position/j1939_vehicle_position_srv.o \
+		$(CC) $(LDFLAGS) $^ $(LDLIBS) -lgps -o $@
 
 isobusfs-srv:	lib.o \
 		libj1939.o \

CMakeLists.txt
@@ -14,6 +14,12 @@ endif()
 
 # Add an option to enable treating warnings as errors
 option(ENABLE_WERROR "Treat all compiler warnings as errors" OFF)
+option(ENABLE_GPS "Enable GPS support" OFF)
+
+find_package(PkgConfig REQUIRED)
+if(ENABLE_GPS)
+  pkg_check_modules(GPS REQUIRED libgps)
+endif()
 
 if(ENABLE_WERROR)
   add_compile_options(-Werror)
@@ -65,6 +71,10 @@ set(PROGRAMS_J1939
 set(PROGRAMS_J1939_TIMEDATE
   j1939-timedate-srv
   j1939-timedate-cli
+)
+
+set(PROGRAMS_J1939_VEHICLE_POSITION
+  j1939-vehicle-position-srv
 )
 
 set(PROGRAMS_ISOBUSFS
@@ -190,6 +200,24 @@ if(NOT ANDROID)
     j1939-timedate-cli
     j1939-timedate-srv
     DESTINATION ${CMAKE_INSTALL_BINDIR})
+
+  if(ENABLE_GPS)
+    set(PUBLIC_HEADER_J1939_VEHICLE_POSITION
+      j1939_vehicle_position/j1939_vehicle_position_cmn.h
+    )
+
+    add_executable(j1939-vehicle-position-srv
+      j1939_vehicle_position/j1939_vehicle_position_srv.c
+    )
+
+    target_link_libraries(j1939-vehicle-position-srv
+      PRIVATE can j1939 ${GPS_LIBRARIES}
+    )
+
+    install(TARGETS
+      j1939-vehicle-position-srv
+      DESTINATION ${CMAKE_INSTALL_BINDIR})
+  endif()
 
 endif()
 

commit 9b5b03087703b24f19f7be11e9b9a04acfac082c

Makefile: remove erroneous '\'
This should at least fix the `No rule to make target` error:

```
cc -O2 -Wall -Wno-parentheses -Wsign-compare -I. -Iinclude -DAF_CAN=PF_CAN -DPF_CAN=29 -DSO_RXQ_OVFL=40 -DSCM_TIMESTAMPING_OPT_STATS=54 -DCLOCK_TAI=11 -DSO_TXTIME=61 -DSCM_TXTIME=SO_TXTIME -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE  -c -o j1939_vehicle_position/j1939_vehicle_position_srv.o j1939_vehicle_position/j1939_vehicle_position_srv.c
j1939_vehicle_position/j1939_vehicle_position_srv.c:7:10: fatal error: gps.h: No such file or directory
    7 | #include <gps.h>
      |          ^~~~~~~
compilation terminated.
make: *** [<builtin>: j1939_vehicle_position/j1939_vehicle_position_srv.o] Error 1
make: *** No rule to make target 'cc', needed by 'j1939-vehicle-position-srv'.
make: *** No rule to make target '-lgps', needed by 'j1939-vehicle-position-srv'.
make: *** No rule to make target '-o', needed by 'j1939-vehicle-position-srv'.
```

Link: https://github.com/linux-can/can-utils/commit/71b2aec834e74d57c643e7a6776f21a1448ef17a#commitcomment-153191516
Fixes: 71b2aec834e7 ("j1939-vehicle-position-srv: Introduce J1939 and NMEA 2000 Vehicle Position Server")

Makefile
@@ -193,7 +193,7 @@ j1939-timedate-cli:	lib.o \
 j1939-vehicle-position-srv: \
 			lib.o \
 			libj1939.o \
-			j1939_vehicle_position/j1939_vehicle_position_srv.o \
+			j1939_vehicle_position/j1939_vehicle_position_srv.o
 		$(CC) $(LDFLAGS) $^ $(LDLIBS) -lgps -o $@
 
 isobusfs-srv:	lib.o \